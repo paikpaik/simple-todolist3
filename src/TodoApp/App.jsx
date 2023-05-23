85d862f7e514a494bb336110eecd9fad9cf40985
paikpaik <woduf9311@gmail.com>
2023-05-24 00:14:12 +0900

    reduxtoolkit을 활용한 todolist

---
+++ src/TodoApp/App.jsx
@@ -0,0 +1,202 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, Provider, useSelector } from 'react-redux'
+import { createSelector, configureStore, createSlice } from "@reduxjs/toolkit";
+
+const capitalize = (s) => s.charAt(0).toUpperCase().concat(s.slice(1));
+const filterList = ["all", "completed", "todo"];
+
+const fetchTodos = () =>
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((data) => data.json())
+    .then((todos) => todos.slice(0, 3));
+
+const initialState = {
+  todos: [],
+  filter: "all",
+  globalId: 3000,
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    // reducers를 완성.
+    changeFilter(state, action){
+      state.filter = action.payload
+    },
+    initializeTodos(state, action){
+      state.todos = action.payload
+    },
+    addTodo(state, action){
+      state.todos.unshift({
+        title: action.payload,
+        id: state.globalId
+      })
+      state.globalId++
+    },
+    deleteTodo(state, action){
+      const index = state.todos.findIndex(todo => todo.id === action.payload)
+      state.todos.splice(index, 1)
+    },
+    toggleTodo(state, action){
+      const todo = state.todos.find(todo => todo.id === action.payload)
+      todo.completed = !todo.completed
+    },
+  },
+});
+
+const store = configureStore({ reducer: todosSlice.reducer });
+
+// useDispatch와 slice에서 생성한 액션을 활용해 action creator를 생성.
+const actions = todosSlice.actions
+
+function useTodoActions() {
+  const dispatch = useDispatch()
+  const toggleTodo = useCallback(
+    (id) => dispatch(actions.toggleTodo(id)),
+    [dispatch]
+  );
+  const deleteTodo = useCallback(
+    (id) => dispatch(actions.deleteTodo(id)),
+    [dispatch]
+  );
+  const addTodo = useCallback(
+    (title) => dispatch(actions.addTodo(title)),
+    [dispatch]
+  );
+  const changeFilter = useCallback(
+    (filter) => dispatch(actions.changeFilter(filter)),
+    [dispatch]
+  );
+  const initializeTodos = useCallback(
+    (todos) => dispatch(actions.initializeTodos(todos)),
+    [dispatch]
+  );
+
+  return {
+    toggleTodo,
+    deleteTodo,
+    addTodo,
+    changeFilter,
+    initializeTodos,
+  };
+}
+
+function TodoApp() {
+  // Provider에 store를 추가.
+  return (
+    <Provider store={store}>
+      <TodosPage />
+    </Provider>
+  );
+}
+
+export default TodoApp;
+
+function TodosPage() {
+  // redux의 action creator를 사용하도록 메서드를 추가.
+  const { initializeTodos } = useTodoActions();
+
+  useEffect(() => {
+    fetchTodos().then(initializeTodos);
+  }, [initializeTodos]);
+
+  return (
+    <div>
+      <TodoForm />
+      <TodoFilter />
+      <TodoList />
+    </div>
+  );
+}
+
+function TodoForm() {
+  // redux의 action creator를 사용하도록 메서드를 추가.
+  const { addTodo } = useTodoActions();
+  const [title, setTitle] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(title);
+        setTitle("");
+      }}
+    >
+      <label htmlFor="todo-title">Title</label>
+
+      <input
+        id="todo-title"
+        type="text"
+        name="todo-title"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+
+      <button type="submit">Make</button>
+    </form>
+  );
+}
+
+const filteredTodosSelector = createSelector(
+  state => state.todos,
+  state => state.filter,
+  (todos, filter) => {
+    return todos.filter((todo) => {
+      return (
+        filter === "all" ||
+        (filter === "completed" && todo.completed) ||
+        (filter === "todo" && !todo.completed)
+      );
+    });
+  }    
+)
+
+function TodoList() {
+  // redux의 action creator를 사용하도록 메서드를 추가.
+  const { toggleTodo, deleteTodo } = useTodoActions();
+  const filteredTodos = useSelector(filteredTodosSelector)
+
+  const handleDeleteTodo = (id) => (e) => {
+    e.stopPropagation();
+    deleteTodo(id);
+  };
+
+  return (
+    <ul>
+      {filteredTodos.map(({ title, completed, id }) => (
+        <li key={id} onClick={() => toggleTodo(id)}>
+          <h5>{title}</h5>
+          <div>
+            {completed ? "☑️ " : "✏️ "}
+            <button onClick={handleDeleteTodo(id)}>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function TodoFilter() {
+  const { changeFilter } = useTodoActions();
+  // context가 아닌 redux store에서 데이터를 얻어옴.
+  const filter = useSelector(state => state.filter)
+
+  return (
+    <div>
+      <label htmlFor="filter">Filter</label>
+      <select
+        onChange={(e) => changeFilter(e.target.value)}
+        id="filter"
+        name="filter"
+        value={filter}
+      >
+        {filterList.map((filterText) => (
+          <option key={filterText} value={filterText}>
+            {capitalize(filterText)}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
